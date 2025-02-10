52cba19f0631a4bc6e23dac9f651fa117dfc5581
Aniket <49784626+abrcode@users.noreply.github.com>
2025-02-10 20:58:32 +0530

    DFS related question
    - is balanced tree or not

---
+++ DFS-DSA.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+
+public class TreeNode {
+     public var val: Int
+     public var left: TreeNode?
+     public var right: TreeNode?
+     public init() { self.val = 0; self.left = nil; self.right = nil; }
+     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+         self.val = val
+         self.left = left
+         self.right = right
+     }
+ }
+
+
+
+/*
+  Balanced binary tree :- https://leetcode.com/problems/balanced-binary-tree/description/
+ */
+
+func isBalanced(_ root: TreeNode?) -> Bool {
+ 
+    guard let root = root else { return true }
+    
+    
+    if !isBalanced(root) || !isBalanced(root) {
+        return false
+    }
+    
+    
+    func val(_ head : TreeNode?) -> Int {
+        
+        guard let head = head else { return -1 }
+
+        return 1 + max(val(head.left), val(head.right))
+        
+    }
+    
+    return abs(val(root.left) - val(root.right)) <= 1
+    
+}
+
+let root = TreeNode(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(2, nil, TreeNode(4)))
+print("Balance Tree :->  \(isBalanced(root))")
